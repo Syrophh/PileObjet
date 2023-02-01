f71439d37cf79a51748e785e6df6942d7ad00571
Thomas Bance-Laupenie <thomas170303@gmail.com>
2023-02-01 11:50:53 +0100

    début utilitaire

---
+++ PileObjet/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilitaires
+{
+    public abstract class Utilitaire
+    {
+        public static int SaisirNb(int pMin)
+        {
+            int nb = 0;
+            do
+            {
+                try
+                {
+                    Console.Write("Veuillez saisir un nombre entier supérieur à " + pMin + " : ");
+                    nb = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException) { }
+            } while (nb < pMin);
+            return nb;
+        }
+    }
+}
